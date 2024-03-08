037ac18d908c24bca7f09173107cdd73d3b5f6ac
dc-dc-dc <dgcruz983@gmail.com>
2024-03-08 13:57:13 -0500

    lower tf version

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ setup(
     python_requires=">=3.8",
     packages=find_packages(where="mlxlite"),
     install_requires=["mlx", "flatbuffers"],
-    extras_require={"test": ["tensorflow==2.15.0"]},
+    extras_require={"test": ["tensorflow==2.13.0"]},
     include_package_data=True
 )
